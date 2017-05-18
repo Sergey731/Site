8d873499f55c1204759f76a86cca583c50d4a23f
Sergey731 <psv.731@yandex.ru>
2017-05-18 20:19:02 +0500

    Удалены комментарии

--- Site/result.php
+++ Site/result.php
@@ -15,32 +15,12 @@ catch (PDOexeption $e) {
     echo "Невозможно установить соединение с базой данных";
 }
 
-/*$query = "SELECT * FROM emales";
-$data = $pdo->query($query);
-
-while ($emales=$data->fetch()) {
-    echo $emales['emale'] . "<br />";    
-}*/
-
-
-
 
 $query = "INSERT INTO emales VALUES ('$e_mail')";
 $data = $pdo->query($query);
 echo "Спасибо. Теперь вы ничего не пропустите.";
 
-/*$file="../Bases/base.txt";
-$text=file($file, FILE_IGNORE_NEW_LINES); 
-$e_mail=$_POST["email"];
 
-if (in_array($e_mail, $text)) {	
-	echo "Спасибо. Вы уже зарегистрированы.";
-} else{
-	$f=fopen($file,"a+t");
-	fputs($f,$e_mail . "\n");
-	fclose($f);
-	echo "Спасибо. Теперь вы ничего не пропустите.";
-}*/
 ?>
 <?php include ("menu.php"); ?> 
 </body>
