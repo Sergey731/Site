9746a8c8b4d9f05e7d30622f5b1e4c5e388d03ca
Sergey731 <psv.731@yandex.ru>
2017-05-19 10:56:46 +0500

    Запись в Mysql

--- Site/result.php
+++ Site/result.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <title>Document</title>
 </head>
 <body>
 <?php
@@ -11,20 +11,34 @@ $e_mail=$_POST["email"];
 try {
 $pdo = new PDO('mysql:host=localhost;dbname=base', 'root', 'carlsen');
 }
-catch (PDOexeption $e) {
+catch (PDOException $e) {
     echo "Невозможно установить соединение с базой данных";
 }
 
-
-$query = "INSERT INTO emales VALUES ('$e_mail')";
+$query = "SELECT * FROM emales";
 $data = $pdo->query($query);
-echo "Спасибо. Теперь вы ничего не пропустите.";
+$counter = 0;
 
-
+    while ($emales=$data->fetch()) {        
+        if ($emales['emale'] == $e_mail) {
+            $counter+=1;                                       
+        } 
+        if ($counter==1) {
+            break;
+        }
+    }
+    
+    if ($counter>=1) {
+        echo "Спасибо. Вы уже зарегистрированы.";        
+    } else {
+        $query = "INSERT INTO emales VALUES ('$e_mail')";
+        $data = $pdo->query($query);
+        echo "Спасибо. Теперь вы ничего не пропустите.";
+        }   
 ?>
 <?php include ("menu.php"); ?> 
 </body>
 </html>
 
 
-	
+    
